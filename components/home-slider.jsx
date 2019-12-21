8a12379b4a83ee59a8c7b42e911db74979f00b66
ASahak <arthur.sahakyan96@gmail.com>
2019-12-21 22:25:53 +0400

    finish slider

--- components/home-slider.jsx
+++ components/home-slider.jsx
@@ -6,14 +6,20 @@ export default class HomeSlider extends React.Component {
         this.state = {
             timer: 0,
             activeIndex: 0,
-            dotsCount: null
+            dotsCount: null,
+            styleWrapper: {}
         };
         this.images = ['slider1', 'slider2', 'slider3'];
         this._slide = this._slide.bind(this);
+        this.touchStart = this.touchStart.bind(this);
+        this.touchMove = this.touchMove.bind(this);
+        this.touchEnd = this.touchEnd.bind(this);
         this.getSliderPosition = this.getSliderPosition.bind(this);
-        this.styleWrapper = {};
         this.slider = React.createRef();
+        this.svg = React.createRef();
         this.autoPlayInterval = null;
+        this.progressSpinner = null;
+        this.touchX = 0;
     }
     UNSAFE_componentWillMount() {
         if (this.props.dots) {
@@ -21,25 +27,79 @@ export default class HomeSlider extends React.Component {
                 dotsCount: this.images.length
             })
         }
-        this.styleWrapper = {};
-        Object.assign(this.styleWrapper, {
-            'transform': 'translate3d(0px, 0px, 0px)'
+        this.setState({
+            styleWrapper: {'transform': 'translate3d(0px, 0px, 0px)'}
         })
-
+    }
+    handleVisibilityChange(hidden) {
+        if (document[hidden]) {
+            clearInterval(this.progressSpinner);
+            clearInterval(this.autoPlayInterval);
+        } else {
+            clearInterval(this.progressSpinner);
+            clearInterval(this.autoPlayInterval);
+            this.IntervalProgress();
+            this.AutoPlayWithTimer(this.state.timer);
+        }
     }
 
     componentDidMount() {
+        let hidden, visibilityChange;
+        if (typeof document.hidden !== 'undefined') { // Opera 12.10 and Firefox 18 and later support
+            hidden = "hidden";
+            visibilityChange = 'visibilitychange';
+        } else if (typeof document.msHidden !== 'undefined') {
+            hidden = 'msHidden';
+            visibilityChange = 'msvisibilitychange';
+        } else if (typeof document.webkitHidden !== 'undefined') {
+            hidden = 'webkitHidden';
+            visibilityChange = 'webkitvisibilitychange';
+       }
+
+        if (typeof document.addEventListener === "undefined" || hidden === undefined) {
+            console.log("This demo requires a browser, such as Google Chrome or Firefox, that supports the Page Visibility API.");
+        } else {
+            // Handle page visibility change
+            document.addEventListener(visibilityChange, this.handleVisibilityChange.bind(this, hidden), false);
+        }
+
         // AutoPlay
         if (this.props.autoplay) {
             let timer = this.props.timer || 3000;
             this.setState({
                 timer: timer
+            }, () => {
+                this.IntervalProgress();
+                this.AutoPlayWithTimer(timer);
             })
-            this.AutoPlayWithTimer(timer);
         }
+    }
+    touchStart (event) {
+        this.touchX = event.touches[0].pageX;
+    }
+    touchMove (event) {
+        clearInterval(this.progressSpinner);
+        clearInterval(this.autoPlayInterval);
+        this.getSliderPosition('touchMove', event.touches[0].pageX)
+    }
+    touchEnd (event) {
+        clearInterval(this.progressSpinner);
+        clearInterval(this.autoPlayInterval);
+        this.getSliderPosition('touchEnd', event.changedTouches[0].pageX)
+    }
+    IntervalProgress () {
+        let percentage = 0;
+        clearInterval(this.progressSpinner);
+        this.progressSpinner = setInterval(() => {
+            if (this.svg.current && percentage < 100) {
+                percentage+=1;
+                this.svg.current.style.background = 'conic-gradient(#f01b48 '+ percentage + '%, 0, #f4f4f4 66%)';
+            }
+        }, this.state.timer / 100)
     }
     AutoPlayWithTimer (timer) {
         this.autoPlayInterval = setInterval(() => {
+            this.IntervalProgress();
             let _activeIndex = this.state.activeIndex;
             if (_activeIndex === this.state.dotsCount - 1) _activeIndex = 0;
             else  _activeIndex++;
@@ -58,20 +118,56 @@ export default class HomeSlider extends React.Component {
         )
     }
     _slide (index) {
-        clearInterval(this.autoPlayInterval);
-        this.setState({
-            activeIndex: index
-        }, () => {
-            this.props.autoplay && this.AutoPlayWithTimer(this.state.timer);
-        })
+        if (index !== this.state.activeIndex) {
+            clearInterval(this.progressSpinner);
+            this.IntervalProgress();
+            clearInterval(this.autoPlayInterval);
+            this.setState({
+                activeIndex: index
+            }, () => {
+                this.getSliderPosition()
+                this.IntervalProgress();
+                this.props.autoplay && this.AutoPlayWithTimer(this.state.timer);
+            })
+        }
     }
-    getSliderPosition () {
+    getSliderPosition (type, touchX) {
         let widthSlider = this.slider.current && this.slider.current.getBoundingClientRect().width || 0;
-        this.styleWrapper = {};
-        Object.assign(this.styleWrapper, {
-            'transform': 'translate3d(' + -(widthSlider * this.state.activeIndex) + 'px, 0px, 0px)'
-        })
-        return this.styleWrapper
+        if (type === 'touchMove') {
+            this.setState({
+                styleWrapper: {
+                    'transition': '0s',
+                    'transform': 'translate3d(' + -(this.touchX - touchX + (widthSlider * this.state.activeIndex)) + 'px, 0px, 0px)'}
+            })
+        } else if (type === 'touchEnd') {
+            if (this.touchX > touchX) {
+                let ind = this.state.activeIndex;
+                if (ind === this.state.dotsCount - 1) ind = this.state.dotsCount - 1;
+                else ind++;
+                this.setState({
+                    activeIndex: ind
+                }, () => {
+                    this.getSliderPosition()
+                    this.IntervalProgress();
+                    this.AutoPlayWithTimer(this.state.timer);
+                });
+            } else {
+                let ind = this.state.activeIndex;
+                if (ind === 0) ind = 0;
+                else ind--;
+                this.setState({
+                    activeIndex: ind
+                }, () => {
+                    this.getSliderPosition()
+                    this.IntervalProgress();
+                    this.AutoPlayWithTimer(this.state.timer);
+                });
+            }
+        } else {
+            this.setState({
+                styleWrapper: {'transform': 'translate3d(' + -(widthSlider * this.state.activeIndex) + 'px, 0px, 0px)'}
+            })
+        }
     }
     render () {
         const Dots = () => {
@@ -86,11 +182,18 @@ export default class HomeSlider extends React.Component {
                 </div>
             )
         };
-
+        const Spinner = () => {
+            return (
+                <div className="spinner_wrapper">
+                    <div className="circle" ref={this.svg}></div>
+                </div>
+            )
+        };
         return (
             <div className={classes['main-slider']} ref={this.slider}>
-                <div className={classes['main-slider__container']}>
-                    <div className={classes['main-slider__wrapper']} style={this.getSliderPosition()}>
+                <div className={classes['main-slider__container']} onTouchStart={this.touchStart} onTouchMove={this.touchMove} onTouchEnd={this.touchEnd}>
+                    {(this.props.autoplay) && <Spinner />}
+                    <div className={classes['main-slider__wrapper']} style={this.state.styleWrapper}>
                         {this.images.map(this.Slide)}
                     </div>
                 </div>
