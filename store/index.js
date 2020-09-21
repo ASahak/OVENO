138ff86504a489b663eae0bfdfafea5affa08893
ASahak <arthur.sahakyan96@gmail.com>
2020-09-21 23:33:44 +0400

    commit 10

--- store/index.js
+++ store/index.js
@@ -1,8 +1,7 @@
 import rootReducer from 'store/reducers';
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import { _rootSaga } from 'store/saga';
-const composeEnhancers = compose;
 
 const state = {
     auth: {
@@ -15,7 +14,7 @@ const state = {
 
 const sagaMiddleware = createSagaMiddleware();
 export function configureStore() {
-    const store = createStore(rootReducer, composeEnhancers(applyMiddleware(sagaMiddleware)));
+    const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
     return store;
 }
 const store = configureStore();
