dcf9cb5fe80ab900364c65b8f5be355948303800
ASahak <arthur.sahakyan96@gmail.com>
2020-12-15 23:34:04 +0400

    chnaged location of email auth

--- server/config/index.js
+++ server/config/index.js
@@ -4,6 +4,8 @@ dotenv.config();
 module.exports = {
     dbURI: process.env.MONGODB_URI || 'mongodb://localhost:27017/oveno',
     port: process.env.PORT,
+    email_auth_user: process.env.EMAIL_AUTH_USER,
+    email_auth_password: process.env.EMAIL_AUTH_PASSWORD,
     secretKey: process.env.SECRET,
     clientURL: process.env.CLIENT_URL,
     secretKeyResetPassword: process.env.SECRET_RESET_PASSWORD,
